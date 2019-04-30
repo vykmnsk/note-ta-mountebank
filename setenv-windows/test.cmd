46aad88e57adaa13f2bae6ded15075e39f1b645f
Yuri Klind <vykmnsk@gmail.com>
2019-04-30 20:41:00 +1000

    Add set environment vars example

---
+++ setenv-windows/test.cmd
@@ -0,0 +1,11 @@
+set TESTAPI_ENV=test
+set TESTAPI_WEB_USR=user@testmail.com
+set TESTAPI_WEB_PWD=testautomation
+REM set TESTAPI_WEB_USR=testuser
+REM set TESTAPI_WEB_PWD=testpassword
+
+rem For test user creation
+set TESTAPI_TESTING_AUTH=77339ed3
+set TESTAPI_COMPANY_ID=649b2727d3
+set TESTAPI_MANAGER_ID=adddc8c8
+set TESTAPI_APP_USER_MOBILE=0400011222
